337a505a4465a672985871ae17ee4fc0c2150185
DiegoCao <945003692@qq.com>
2021-09-20 10:52:51 -0400

    change

--- process_2.py
+++ process_2.py
@@ -129,7 +129,7 @@ if __name__ == "__main__":
 
     # dfcount = res.na.fill(0).groupby("rid").agg(func.sum(res.commentemojicnt+res.premojicnt+res.issueemojicnt).alias('totalcnt'))
 
-    res.write.format("csv").option("header", "true").save("/user/hangrui/new/restype")
+    res.write.format("csv").option("header", "true").save("/user/hangrui/new/restypenew")
     # dfcount = dfcount.join(df_event_cnt, df_event_cnt['rid']==dfcount['rid'])
     # dfcount = dfcount.join(dfusers, dfusers['rid']==dfcount['rid'])
 
