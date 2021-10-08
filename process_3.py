2a919bcc8a89882f6b6034aa4545b164fa880d79
DiegoCao <945003692@qq.com>
2021-10-08 02:46:42 -0400

    change

--- process_3.py
+++ process_3.py
@@ -58,12 +58,13 @@ if __name__ == "__main__":
     issueemoji = df.groupby('issueid').agg(func.collect_list('emojis').alias("issueemoji"))
 
     commentemoji = df.groupby('commentid').agg(func.collect_list('emojis').alias("commentemoji"))
+    myudf = func.udf(getSetlen)
+    issueemoji = issueemoji.select("issueid", myudf("issueemoji").alias("issueemojicnt"))
+    commentemoji = commentemoji.select("commentid", myudf("commentemoji").alias("commentemojicnt"))
     issueemoji.write.format("csv").option("header", "true").save("/user/hangrui/new/issuecntemoji")
     commentemoji.write.format("csv").option("header", "true").save("/user/hangrui/new/commentcntemoji")
 
-    # myudf = func.udf(getSetlen)
-    # issueemoji = issueemoji.select("issueid", myudf("issueemoji").alias("issueemojicnt"))
-    # commentemoji = commentemoji.select("commentid", myudf("commentemoji").alias("commentemojicnt"))
+
 
     # # df.show()
     # issueemoji.show()
