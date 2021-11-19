9d1ce9fcec0a17a962b7eb28184ba886b682ff62
DiegoCao <945003692@qq.com>
2021-11-19 03:09:10 -0500

    change

--- process_3.py
+++ process_3.py
@@ -47,7 +47,7 @@ if __name__ == "__main__":
     sc_name = "Conversation Issue"
     sc = SparkContext(conf=SparkConf().setAppName(sc_name))
     sc.addFile("./emoji-test.txt")
-    sc.setLogLevel('WARN')
+    sc.setLogLevel('ERROR')
     spark = SparkSession(sc)
     spark.sparkContext.setLogLevel('WARN')
     raw_root = "/user/hangrui/"
@@ -55,16 +55,16 @@ if __name__ == "__main__":
     df = spark.read.parquet("/user/hangrui/2018_parquet_v3.parquet")
     print('the original number of rows: ', df.count())
 
+
     res = df.select('commentissueid', 'rid').distinct()
 
-    res.write.format("csv").option("header", "true").save("/user/hangrui/conversation/issueridmap")
 
-    # issueemoji = df.groupby('issueid').agg(func.collect_list('emojis').alias("issueemoji"))
+    issueemoji = df.groupby('issueid').agg(func.collect_list('emojis').alias("issueemoji"))
 
-    # commentemoji = df.groupby('commentid').agg(func.collect_list('emojis').alias("commentemoji"))
-    # myudf = func.udf(getSetlen)
-    # issueemoji = issueemoji.select("issueid", myudf("issueemoji").alias("issueemojicnt"))
-    # commentemoji = commentemoji.select("commentid", myudf("commentemoji").alias("commentemojicnt"))
+    commentemoji = df.groupby('commentid').agg(func.collect_list('emojis').alias("commentemoji"))
+    myudf = func.udf(getSetlen)
+    issueemoji = issueemoji.select("issueid", myudf("issueemoji").alias("issueemojicnt"))
+    commentemoji = commentemoji.select("commentid", myudf("commentemoji").alias("commentemojicnt"))
     # issueemoji.write.format("csv").option("header", "true").save("/user/hangrui/new/issuecntemoji")
     # commentemoji.write.format("csv").option("header", "true").save("/user/hangrui/new/commentcntemoji")
 
@@ -80,44 +80,45 @@ if __name__ == "__main__":
     #         .join(commentemoji.alias('c'), commentemoji.commentid==df.commentid, how='outer')\
     #             .select('a.rid', 'a.commentid', 'a.issueid', 'b.issueemojicnt', 'c.commentemojicnt')
     
-    df.write.format("csv").option("header", "true").save("/user/hangrui/new/conversation_new")
-    print('the row number is :', df.count())
+    # df.write.format("csv").option("header", "true").save("/user/hangrui/new/conversation_new")
+    # print('the row number is :', df.count())
 
 
 
 
-    # df = df.filter(df.commentid.isNotNull()&df.commentissueid.isNotNull())
-    # dfissue = df_old.filter(df_old.issueid.isNotNull())
+    df = df.filter(df.commentid.isNotNull()&df.commentissueid.isNotNull())
+    dfissue = df_old.filter(df_old.issueid.isNotNull())
 
     
-    # def sorter(l):
-    #     res = sorted(l, key=operator.itemgetter(0))
-    #     return [item[1] for item in res]
-    # sort_udf = func.udf(sorter)
+    def sorter(l):
+        res = sorted(l, key=operator.itemgetter(0))
+        return [item[1] for item in res]
+        
+    sort_udf = func.udf(sorter)
 
-    # dfi = dfissue.groupby('issueid')\
-    #     .agg(func.collect_list(func.struct("created_time", "has_emoji"))\
-    #     .alias("templist"))
+    dfi = dfissue.groupby('issueid')\
+        .agg(func.collect_list(func.struct("created_time", "has_emoji"))\
+        .alias("templist"))
 
     # # w = Window.partitionby()
-    # # dfi = dfi.groupby('issueid')\
-    # #     .agg(func.collect_list(func.struct("created_time", "has_emoji"))\
-    # #     .alias("templist"))
+    dfi = dfi.groupby('issueid')\
+        .agg(func.collect_list(func.struct("created_time", "has_emoji"))\
+        .alias("templist"))
     
-    # dfi = dfi.select("issueid", sort_udf("templist") \
-    #     .alias("comment_list")) \
+    dfi = dfi.select("issueid", sort_udf("templist") \
+        .alias("comment_list")) \
         
     # dfi.show()
 
-    # dfci = df.groupby('commentissueid')\
-    #         .agg(func.collect_list(func.struct("created_time", "has_emoji"))\
-    #         .alias("templist"))
+    dfci = df.groupby('commentissueid')\
+            .agg(func.collect_list(func.struct("created_time", "has_emoji"))\
+            .alias("templist"))
     
-    # dfci = dfci.select("commentissueid", sort_udf("templist").alias("comment_lis"))
+    dfci = dfci.select("commentissueid", sort_udf("templist").alias("comment_lis"))
 
-    # dfnew = dfci.join(dfi, dfi.issueid==dfci.commentissueid, 'outer')
+    dfnew = dfci.join(dfi, dfi.issueid==dfci.commentissueid, 'outer')
 
-    # dfnew.write.format("csv").option("header", "true").save("/user/hangrui/new/conver")
+    dfnew.write.format("csv").option("header", "true").save("/user/hangrui/new/conver")
     # ç
 
     # df.show()
