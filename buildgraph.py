4a8ed7d2954e0ce24d5bc7c4fe045f4ba0548555
DiegoCao <945003692@qq.com>
2022-02-16 21:49:26 -0500

    change

--- buildgraph.py
+++ buildgraph.py
@@ -278,7 +278,7 @@ if __name__ == "__main__":
     spark.sparkContext.setLogLevel('WARN')
     raw_root = "/user/hangrui/"
     df_old = spark.read.parquet("/user/hangrui/2018_month_new.parquet")
-    df = spark.read.parquet("/user/hangrui2018_month_new.parquet")
+    df = spark.read.parquet("/user/hangrui/2018_month_new.parquet")
     # df_old = spark.read.parquet("/user/hangrui/2018_day_new.parquet")
     # df = spark.read.parquet("/user/hangrui/2018_day_new.parquet")
     print('the df head is ', df.head())
